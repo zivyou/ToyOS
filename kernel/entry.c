5e79bd6eab7d89991b263c55ae98d5c438c1e7b9
zivyou <root@ziv.hypervisor>
2018-08-06 12:36:46 -0400

    fixed untracked files

--- kernel/entry.c
+++ kernel/entry.c
@@ -4,24 +4,22 @@
 
 int kern_entry(){
     const uint32_t VGA_ADDR = (uint32_t )0xB8000;
-    const uint8_t COLOR = 0x61;
+    const uint8_t COLOR = (0 << 4) | (15 & 0x0F);
+    const uint16_t blank = (uint16_t)COLOR << 8 | 0x20;
     
-    uint8_t *dst = (uint8_t *)VGA_ADDR;
+    uint16_t *dst = (uint16_t *)VGA_ADDR;
     uint32_t i=0;
     while (i < 0xFFFF){
-        *(dst+i) = 0;
+        *(dst+i) = blank;
         i++;
     }
+    
+    
     
     char welcome[] = "Welcome to ToyOS kernel world!";
     i = 0;
     while(welcome[i]){
-        uint32_t index = (uint32_t)(i*2);
-        /* This really confused me: if I write like this: *(dst+(uint32_t)(i*2)) = welcome[i];
-         * the code just doesnot work. What the fucking problem with gcc? 
-         */
-        *(dst+(index)) = welcome[i];
-        *(dst+index+1) = COLOR;
+        dst[i] = (uint16_t)welcome[i] | (uint16_t)COLOR<<8;
         i++;
     }
     
---
+++ lib/terminal.c
@@ -0,0 +1,8 @@
+#include "terminal.h"
+
+void terminal_init2(){
+    int i = 0;
+    while (i < 100)
+        i++;
+    
+}
